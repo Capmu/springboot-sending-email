9868cce902f8e02f87d8a8e12b6db485a4540a51
Capmu <p.kanawat.capmu@gmail.com>
2023-05-03 23:05:08 +0700

    Add impl & configurations to send an plain email to target user(s)

--- src/main/java/com/capmu/sendingemail/SendingEmailApplication.java
+++ src/main/java/com/capmu/sendingemail/SendingEmailApplication.java
@@ -2,9 +2,32 @@ package com.capmu.sendingemail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
 
 @SpringBootApplication
 public class SendingEmailApplication {
+
+//	@Bean
+//	public JavaMailSender getJavaMailSender() {
+//		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+//		mailSender.setHost("smtp.gmail.com");
+//		mailSender.setPort(587);
+//
+//		mailSender.setUsername("<email-for-smtp>");
+//		mailSender.setPassword("<password-for-smtp>"); //generated from https://myaccount.google.com/apppasswords
+//
+//		Properties props = mailSender.getJavaMailProperties();
+//		props.put("mail.transport.protocol", "smtp");
+//		props.put("mail.smtp.auth", "true");
+//		props.put("mail.smtp.starttls.enable", "true");
+//		props.put("mail.debug", "true");
+//
+//		return mailSender;
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SendingEmailApplication.class, args);
--- src/main/java/com/capmu/sendingemail/contoller/ReportController.java
+++ src/main/java/com/capmu/sendingemail/contoller/ReportController.java
@@ -1,14 +1,25 @@
 package com.capmu.sendingemail.contoller;
 
+import com.capmu.sendingemail.service.EmailService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class ReportController {
+
+    private final EmailService emailService;
 
     @GetMapping("/report")
     public ResponseEntity<String> sendEmailReport() {
+
+        emailService.sendSimpleMessage(
+                "capmu.explore@gmail.com",
+                "Test sending an email using Spring Boot",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        );
 
         return ResponseEntity.ok("sent!");
     }
